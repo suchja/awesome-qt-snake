a91ca2b8a6f79f74db5757d14513ca1c46bfaa94
Jan (LernMoment.de) <jan@lernmoment.de>
2024-06-16 14:35:06 +0200

    Adds SnakeCore as library for model
    this will be the basis for the implementation of #5.

---
+++ src/snakecore/game.cpp
@@ -0,0 +1,10 @@
+#include "game.h"
+
+#include <QDebug>
+
+Game::Game() {}
+
+void Game::startGame()
+{
+    qDebug() << "Game::startGame called!";
+}
---
+++ src/snakecore/game.h
@@ -0,0 +1,14 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include <QObject>
+
+class Game
+{
+public:
+    Game();
+
+    void startGame();
+};
+
+#endif // GAME_H
